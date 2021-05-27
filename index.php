ffe432e24afdf3a29549507eef5dfba1b5ac75d6
Edouard Nicolas <edouard.nicolas@edu.devinci.fr>
2021-05-27 02:52:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>three js test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- three js ----------------------------------------------------------------------- -->
+    <script src="js/three/build/three.min.js"></script>
+    <script src="js/three/examples/js/controls/OrbitControls.js"></script>
+    <script type="module" src="js/scene.js"></script>
+    <canvas id="c" ></canvas>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ js/scene.js
@@ -0,0 +1,128 @@
+        var mountain
+        var plateau
+        var michel
+        var killian
+        var edouard
+        var pierre
+        var bar
+        import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+        import { OrbitControls } from "./three/examples/jsm/controls/OrbitControls.js";
+        const canvas = document.querySelector('#c');
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+        
+
+        const renderer = new THREE.WebGLRenderer({canvas});
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+        //orbit controller
+        const controls = new OrbitControls( camera, renderer.domElement );
+
+        // const geometry = new THREE.BoxGeometry();
+        // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        // const cube = new THREE.Mesh( geometry, material );
+        // scene.add( cube );
+
+        const loader = new GLTFLoader();
+
+        const light = new THREE.AmbientLight( 0x404040,5 ); // soft white light
+        scene.add( light );
+
+        loader.load('assets/models/plateau.gltf', function (gltf) {
+            plateau = gltf.scene;
+            plateau.scale.set(5,5,5);
+            scene.add(plateau);
+
+        }, undefined, function (error) {
+
+            console.error(error);
+
+        });
+
+        loader.load('assets/models/michel.gltf', function (gltf) {
+            michel = gltf.scene;
+            michel.scale.set(0.15,0.15,0.15);
+            michel.position.x = 0.53;
+            scene.add(michel);
+
+        }, undefined, function (error) {
+
+            console.error(error);
+
+        });
+
+        loader.load('assets/models/killian.gltf', function (gltf) {
+            killian = gltf.scene;
+            killian.scale.set(0.15,0.15,0.15);
+            killian.position.x = -0.55;
+            killian.position.z = -0.85;
+            scene.add(killian);
+
+        }, undefined, function (error) {
+
+            console.error(error);
+
+        });
+        loader.load('assets/models/edouard.gltf', function (gltf) {
+        edouard = gltf.scene;
+        edouard.scale.set(0.15,0.15,0.15);
+        edouard.position.x = -0.70;
+        edouard.position.z = 1;
+        scene.add(edouard);
+        }, undefined, function (error) {
+        console.error(error);
+        });
+
+        loader.load('assets/models/pierre.gltf', function (gltf) {
+        pierre = gltf.scene;
+        pierre.scale.set(0.15,0.15,0.15);
+        pierre.position.x = 0.55;
+        pierre.position.z = 1.05;
+        scene.add(pierre);
+        }, undefined, function (error) {
+        console.error(error);
+        });
+        
+        //chargement pions fin
+
+        loader.load('assets/models/scene.gltf', function (gltf) {
+        bar = gltf.scene;
+        //bar.scale.set(0.5,0.5,0.5);
+        //bar.position.x = 0;
+        //bar.position.z = 0;
+        scene.add(bar);
+        }, undefined, function (error) {
+        console.error(error);
+        });
+
+        //arriere plan montagne
+        loader.load('assets/models/mountain.gltf', function (gltf) {
+            mountain = gltf.scene;
+            mountain.scale.set(3.2,3.2,3.2);
+            mountain.position.x = 2.2;
+            mountain.position.z = 0.03;
+            mountain.position.y = -0.01;
+            scene.add(mountain);
+
+        }, undefined, function (error) {
+
+            console.error(error);
+
+        });
+
+        
+
+
+
+        camera.position.z = 2;
+        camera.position.y = 2;
+        camera.rotation.x = -0.75;
+        controls.update();
+
+        const animate = function () {
+            requestAnimationFrame(animate);
+            controls.update();
+            renderer.render(scene, camera);
+        };
+
+        animate();
